e60c2ff997f9176ad490e9bddd5751400efa97b7
razanaboelnaga123 <banafsegm@gmail.com>
2025-08-05 19:10:47 +0300

    Created new file to task 10

---
+++ Task10 (1).py
@@ -0,0 +1,32 @@
+# step 1 : Start the dictionary
+treasure = {
+    "clues": ["beach", "cave", "waterfall"],
+    "locations": {
+        "beach": {
+            "items": ["compass", "shovel"],
+            "hint": "dig here"
+        },
+        "cave": {
+            "items": ["lantern", "gold coin"],
+            "hint": "look behind rocks"
+        }
+    },
+    "crew": ["First Mate", "Navigator"]
+}
+
+# step 2 : Make changes to the dictionary
+treasure["crew"].append("cook")
+treasure["locations"]["volcano"] = {
+    "items":["diamond", "lava boots"],
+    "hint":"wear protection"
+}
+treasure["clues"].append("valcano")
+treasure["crew"].remove("Navigator")
+
+# step 3 : Look for gold
+for location, info in treasure["locations"].items():
+    if "gold coin" in ["location"]:
+        print(f"Treasure! In location_name")
+# Count and list all items
+for location, info in treasure["locations"].items():
+    print(f"{location} : {' , '.join(info['items'])}")
---
+++ Task10 (2).py
@@ -0,0 +1,45 @@
+# Word Flipper Mission!
+sentence = "The sky is blue"
+flipped = ' '.join([word[::-1]for word in sentence.split()])
+print(flipped)
+
+
+# Help Desk Hero
+customers = [
+    ("Alex", "VIP"),
+    ("Bob", "Regular"),
+    ("Sarah", "Regular"),
+    ("Maria", "VIP"),
+    ("Mike", "Regular")
+]
+vip_queue = []
+regular_queue = []
+for name, status in customers:
+    if status == "VIP":
+        vip_queue.append(name)
+    else:
+        regular_queue.append(name)
+order = vip_queue + regular_queue
+print("Serve Order:", order)
+
+#School Club Signup Mission
+signups = [
+    ("Liam", "Science"),
+    ("Emma", "Art"),
+    ("Olivia", "Science"),
+    ("Liam", "Art"),  # Duplicate - ignore this one!
+    ("Noah", "Art"),
+    ("Emma", "Science")  # Duplicate - ignore this one too!
+]
+science_club = set()
+art_club = set()
+already_signed_up = set()
+for name, club in signups:
+    if name is not already_signed_up:
+        already_signed_up.add(name)
+        if club == "Science":
+            science_club.add(name)
+        else:
+            art_club.add(name)
+print("Science Club:", science_club)
+print("Art Club:", art_club)
